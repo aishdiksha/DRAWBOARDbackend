9b8daf76b19f7d1a5dfd1a8cf53930f0d0667494
aiswarya <aishdiksha@gmail.com>
2022-07-06 12:00:55 +0530

    commit

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const app = express();
 const userRouter = require("./routers/userRouter");
-const{createServer}=require('http');
-const { Server }=require('socket.io');
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 const httpServer = createServer(app);
-const io= new Server(httpServer,{cors:{
-  origin:['http://localhost:3000']
-}});
-//update send for event send 
-//update rcieving for event recieve
-io.on('connection', (socket) => {
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000","http://192.168.2.172:3000"],
+  },
+});
+
+// receiving event
+io.on("connection", (socket) => {
   console.log("client connected");
-  //do all operation after connection here
-  socket.on('sendmsg',(msg)=>{
+
+  //  do all operation after connection here
+  socket.on("sendmsg", (msg) => {
     console.log(msg);
-    socket.broadcast.emit('recmsg',msg);
+    msg.sent = false;
+    socket.broadcast.emit("recmsg", msg);
   });
 });
 
 const port = 5000;
-//for reading json data
+
+// for reading JSON data
 app.use(express.json());
 
 app.use("/user", userRouter);
@@ -27,28 +34,28 @@ app.use("/user", userRouter);
 // creating a endpoint for '/'
 app.get("/", (req, res) => {
   // for the server
-  console.log("reqest on /");
-
+  console.log("request on /");
   // for the client
   res.send("Request processed on /");
 });
 
 app.get("/home", (req, res) => {
   // for the server
-  console.log("reqest on /home");
-
+  console.log("request on /home");
   // for the client
   res.send("Request processed on /home");
 });
 
 app.get("/add", (req, res) => {
   // for the server
-  console.log("reqest on /add");
-
+  console.log("request on /add");
   // for the client
   res.send("Request processed on /add");
 });
 
 httpServer.listen(port, () => {
-  console.log("Server started");
+  console.log("server started");
 });
+
+//  swiper library
+// cors = creater origin resource server
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,31 +1,34 @@
 const express = require("express");
-const Model = require("../models/usermodel");
+const Model = require("../models/userModel");
+
 const router = express.Router();
-router. post('/add',(req,res)=>{
-    //console.log('request on /add in userRouter');
-    //res.send('response from userrouter /add');    
-    const data=req.body;
-    console.log(data);
-    new Model(req.body).save()
+
+router.post("/add", (req, res) => {
+  // to extract data from request and save to database
+  const data = req.body;
+  console.log(data);
+
+  new Model(req.body)
+    .save()
     .then((data) => {
-        console.log("user data save");
-        res.status(200).json({message:"success"});
-    }).catch((err) => {
-       console.error(err);
-       res.status(500).json(err); 
+      console.log("user data saved!");
+      res.status(200).json({ message: "success" });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
     });
-   
 });
-router.get('/getall',(req,res)=>{
-    Model.find({username:'leon'})
+
+router.get("/getall", (req, res) => {
+  Model.find({})
     .then((data) => {
-        console.log(data);
-        res.status(200).json(data);
-        
-    }).catch((err) => {
-        console.error(err);
-       res.status(500).json(err); 
-        
+      console.log(data);
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json(err);
     });
-})
-module.exports=router;
+});
+module.exports = router;
